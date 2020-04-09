507ed22f55c7a1e0a018d71f9f7f9544829fa13d
ncooney94 <nick.cooney@gmail.com>
2020-04-09 15:59:13 +1000

    yup

--- problem1.js
+++ problem1.js
@@ -2,31 +2,31 @@
 
 const familyOfFour = (n, reservedSeats) => {
   let cinema = []
-  let s = cinema.join('')
   // below loop constructs a nested array of all available seats in the cinema
   for (let i = 1; n >= i; i++) {
     cinema.push([i, 1], [i, 2], [i,3], [i,4], [i,5], [i,6], [i,7], [i,8], [i,9], [i,10])
   }
   // below methods convert each seat array into a seat number, then combined into one array and keeps only unique seats (removes reserved seats and keeps available seats)
   let newArray = []
- let resArr = reservedSeats.forEach( e => {
-  let num = parseInt(e.join(''))
-  newArray.push(num);
- })
- let cinArr = cinema.forEach( e => {
-   let num = parseInt(e.join(''))
-   newArray.push(num);
- })
- let sortedArr = newArray.sort(( a, b) => {
-   return a - b
- })
-const uniq = sortedArr.filter((i, p) => {
-  return sortedArr.lastIndexOf(i) === sortedArr.indexOf(i);
-})
+  reservedSeats.forEach( e => {
+    let num = parseInt(e.join(''))
+    newArray.push(num);
+  })
+  cinema.forEach( e => {
+    let num = parseInt(e.join(''))
+    newArray.push(num);
+  })
+  let sortedArr = newArray.sort(( a, b) => {
+    return a - b
+  })
+  const unique = sortedArr.filter((i, p) => {
+    return sortedArr.lastIndexOf(i) === sortedArr.indexOf(i);
+  })
+  console.log(unique);
   
   
   //count sequences of 4 with remaining array
-  
+
  
 }
 
